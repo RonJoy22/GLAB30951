173393fd7262bab84fa212ecf7fed6cf246d2956
ronald.joyner-brown <r.brownjoyner@gmail.com>
2024-08-26 14:55:48 -0400

    309.5.2

---
+++ src/main/java/model/Usercontroller.java
@@ -0,0 +1,12 @@
+package model;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Usercontroller {
+    @PostMapping("/users")
+    public void printData(@RequestBody user user) {
+        System.out.println("Printing the user data:"+user);
+    }}
